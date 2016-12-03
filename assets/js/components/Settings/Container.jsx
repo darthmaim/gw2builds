a834d274ecb8b943a4b842f6a29f91ad5241ce4e
darthmaim <git@darthmaim.de>
2016-12-03 21:04:02 +0100

    remove old base panels

--- assets/js/components/Settings/Container.jsx
+++ assets/js/components/Settings/Container.jsx
@@ -5,18 +5,12 @@ import { Header, Panel } from '.';
 class SettingsPanel extends React.Component {
     render() {
         return (
-            <div className="settings-panel">
-                <OverlayPanel
-                    header={<Header {...this.props}/>}
-                    panel={<Panel onLanguageChange={this.props.onLanguageChange}
-                        onGameModeChange={this.props.onGameModeChange}
-                        onProfessionChange={this.props.onProfessionChange}
-                        onRaceChange={this.props.onRaceChange}
-                        {...this.props}
-                        />}
-                    onClose={this.onClose}
-                    />
-            </div>
+            <Panel onLanguageChange={this.props.onLanguageChange}
+               onGameModeChange={this.props.onGameModeChange}
+               onProfessionChange={this.props.onProfessionChange}
+               onRaceChange={this.props.onRaceChange}
+               {...this.props}
+            />
         );
     }
 }
