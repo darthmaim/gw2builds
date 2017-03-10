983e7513039310f75f8f1687648d32ebe0d5ccaf
Eearslya Sleiarion <eearslya@gmail.com>
2017-03-10 15:37:45 -0800

    Remove unneeded props from UtilityBarContainer

--- src/components/Skills/SkillBar/UtilityBar/UtilityBarContainer.jsx
+++ src/components/Skills/SkillBar/UtilityBar/UtilityBarContainer.jsx
@@ -1,12 +1,8 @@
 import { connect } from 'react-redux';
-import { getActiveMainhand, getActiveOffhand, getIsTwoHandedActive } from '~/selectors/skills';
 import { getActiveAttunement } from '~/selectors/skills/mechanic';
 import UtilityBar from './UtilityBar';
 
 const mapStateToProps = (state, ownProps) => ({
-    mainhand: getActiveMainhand(state, ownProps),
-    offhand: getActiveOffhand(state, ownProps),
-    isTwoHanded: getIsTwoHandedActive(state, ownProps),
     attunement: getActiveAttunement(state, ownProps),
     skills: state.skills
 });
