ee67c74df381a8cf79e0a71a5f1a557cbb3a3a8d
darthmaim <git@darthmaim.de>
2018-04-13 16:48:11 +0200

    Fail builds in production env with status ≠ 0
    Travis builds were succeding even when babel encountered an errror.

--- build/utils.js
+++ build/utils.js
@@ -3,6 +3,10 @@ import gutil from 'gulp-util';
 export function logError(error) {
     gutil.log(error);
     this.emit('end');
+
+    if(!isDev()) {
+        throw new Error(error);
+    }
 }
 
 export function isDev() {
