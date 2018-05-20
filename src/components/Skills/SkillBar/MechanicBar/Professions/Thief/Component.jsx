82c6b5766b8d9e7d7e08e2e334aff539725cbba9
darthmaim <git@darthmaim.de>
2018-05-20 21:02:00 +0200

    Show correct Thief initiative

--- src/components/Skills/SkillBar/MechanicBar/Professions/Thief/Component.jsx
+++ src/components/Skills/SkillBar/MechanicBar/Professions/Thief/Component.jsx
@@ -4,7 +4,7 @@ import SkillIcon from '../../../../Icon';
 import SkillTooltip from '../../../../../Tooltips/Skills/TooltipContainer';
 import style from './style.css';
 
-const renderSkill = (availableProfessionSkillObjects, availableSkillObjects) => {
+const renderSkill = ({ availableProfessionSkillObjects, availableSkillObjects }) => {
     const possibleSkill = availableProfessionSkillObjects.filter(skill => skill.slot === 'Profession_1')[0];
     const skill = possibleSkill ? availableSkillObjects[possibleSkill.id] : null;
 
@@ -15,12 +15,15 @@ const renderSkill = (availableProfessionSkillObjects, availableSkillObjects) => 
     );
 };
 
-const Thief = ({ availableProfessionSkillObjects, availableSkillObjects }) => (
+const getInitiative = ({ selectedMinorTraitIds }) =>
+    selectedMinorTraitIds.indexOf(1232) !== -1 ? 15 : 12;
+
+const Thief = (props) => (
     <div className={style.component}>
-        {renderSkill(availableProfessionSkillObjects, availableSkillObjects)}
+        {renderSkill(props)}
         <SkillIcon.Empty size={32}/>
         <div className={style.initiative}>
-            {range(0, 15).map(i => (<span key={i}/>))}
+            {range(0, getInitiative(props)).map(i => (<span key={i}/>))}
         </div>
     </div>
 );
--- src/components/Skills/SkillBar/MechanicBar/Professions/Thief/Container.jsx
+++ src/components/Skills/SkillBar/MechanicBar/Professions/Thief/Container.jsx
@@ -1,9 +1,11 @@
 import { connect } from 'react-redux';
 import Thief from './Component';
+import { getSelectedMinorTraitIds } from '../../../../../../selectors/specializations';
 
 const mapStateToProps = (state, ownProps) => ({
     availableSkillObjects: state.availableSkillObjects,
-    availableProfessionSkillObjects: state.availableProfessionSkillObjects
+    availableProfessionSkillObjects: state.availableProfessionSkillObjects,
+    selectedMinorTraitIds: getSelectedMinorTraitIds(state, ownProps)
 });
 
 export default connect(mapStateToProps)(Thief);
