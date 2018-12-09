5bd18e5ad0242c7d504db03781f38ef028ab1596
darthmaim <git@darthmaim.de>
2018-12-09 14:37:49 +0100

    Remove api extensions

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import apiClient from 'gw2api-client';
 import cacheMemory from 'gw2api-client/build/cache/memory';
-import extendApiClient from 'gw2api-extension';
-import extendApiData from 'gw2be-api-extension-data';
+// import extendApiClient from 'gw2api-extension';
+// import extendApiData from 'gw2be-api-extension-data';
 
-export const api = extendApiClient(apiClient(), extendApiData).cacheStorage(cacheMemory());
+export const api = apiClient().cacheStorage(cacheMemory());
